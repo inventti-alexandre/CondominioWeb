ef376399eb8c982d7f88a9b22a154556d0e96446
vhzambrano87 <vhzambrano87@gmail.com>
2018-05-01 11:07:45 -0500

    Maestros V6

--- BuildingProject.Model/ApartmentUser.cs
+++ BuildingProject.Model/ApartmentUser.cs
@@ -5,6 +5,7 @@
         public int apartmentUserID { get; set; }
         public int apartmentID { get; set; }
         public int userID { get; set; }
+        public bool principal { get; set; }
         public virtual Apartment apartment { get; set; }
         public virtual User user { get; set; }
         
