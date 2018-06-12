eb437265397d45bfb1d27d2fb077553e064090fe
victor zambrano <vhzambrano87@gmail.com>
2018-06-11 20:47:45 -0500

    Helper

---
+++ webapp/App_Helpers/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SmartAdminMvc.App_Helpers
+{
+    public static class Helper
+    {
+        public static string DateFormat(DateTime datetime)
+        {
+            string result = "";
+            result = datetime.ToShortDateString();
+            return result;
+        }
+    }
+}
